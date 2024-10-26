93fb1d8453f558eb6145895cb91886b5eb28ec13
Hani <147255323+Hanvalad@users.noreply.github.com>
2024-10-26 23:38:55 +0300

    Исправлены ошибки PEP8 и добавлены docstring'и

--- the_snake.py
+++ the_snake.py
@@ -1,4 +1,4 @@
-from random import randint
+ffrom random import randint
 import pygame
 from typing import Optional, Tuple, List
 
@@ -169,3 +169,4 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
+
